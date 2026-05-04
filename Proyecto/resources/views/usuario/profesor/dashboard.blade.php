05060ef5030af0b514b856777607020ea8866550
Miguel <migueld202035@gmail.com>
2026-05-04 13:18:27 +0200

    Se actualiza el funcionamiento del dashboard, de forma que, al eliminar el módulo y volver al dashboard, muestra el seleccionador en vez de la página de crear módulo

--- Proyecto/resources/views/usuario/profesor/dashboard.blade.php
+++ Proyecto/resources/views/usuario/profesor/dashboard.blade.php
@@ -8,13 +8,17 @@
     
     <p>Dashboard profesor {{ Auth::user()->nombre }}</p>
 
-
-    @if (!$moduloActual)
-        
+    @if (Auth::user()->profesor->modulos->isEmpty())
+        <!-- No tiene ningún módulo -->
         <p>¿Primera vez? Crea un módulo</p>
-        <p><a href="{{ route('profesor.modulos.create') }}">Crear nuevo modulo</a></p>
+        <p><a href="{{ route('profesor.modulos.create') }}">Crear nuevo módulo</a></p>
 
     @else
+        <!-- Tiene módulos pero ninguno seleccionado -->
+        @if (!$moduloActual)
+            <p>Selecciona un módulo</p>
+        @endif
+
         <x-modulo-nav :moduloActual="$moduloActual" />
     @endif
 @endsection
