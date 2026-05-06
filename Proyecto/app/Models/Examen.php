30fffb324a3acdbf5dbb12ca61880743e944f7ab
Miguel Durán <migueld202035@gmail.com>
2026-05-06 21:53:27 +0200

    Se crea el modelo y migración de Examen

---
+++ Proyecto/app/Models/Examen.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Examen extends Model
+{
+    protected $table = 'examenes';
+    protected $primaryKey = 'id_examen';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_examen',
+        'duracion',
+        'fecha_apertura',
+        'id_test',
+    ];
+
+    public function test() {
+        return $this->belongsTo(Test::class, 'id_test', 'id_test');
+    }
+}
